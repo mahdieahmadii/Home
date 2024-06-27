f0031bdda12b95f2b636f22d91eb3c7f163f6cea
Mahdie Ahmadi <mahdieahmadii97@gmail.com>
2024-06-27 19:33:56 +0330

    fix home page

---
+++ components/templates/index/Homes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import db from "../../../data/db.json";
+import Home from "../../modules/Home";
+
+function Homes() {
+  return (
+    <div class="homes">
+      {db.homes.slice(0, 6).map((home) => (
+        <Home key={home.id} {...home} />
+      ))}
+    </div>
+  );
+}
+
+export default Homes;
---
+++ pages/homes/[id].jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingleHomeDetailsPage() {
+  return <div>SingleHomeDetailsPage</div>;
+}
+
+export default SingleHomeDetailsPage;
